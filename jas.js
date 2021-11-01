b94045dd2d299f6b25fad27dd0f67157950cc763
ggroup-pe <93496399+ggroup-pe@users.noreply.github.com>
2021-10-31 19:19:19 -0500

    Create jas.js

---
+++ jas.js
@@ -0,0 +1,95 @@
+    var _0x182c=["head","getElementsByTagName","body","script","createElement","type","text/javascript","src","https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js","async","onload","scrollTop","show","addClass",".ignielToTop","removeClass","scroll","0","animate","html,body","click","block","visible","1","css","style","removeAttr","footer","footer .inner","footer .inner .right",".textrimMenu .inner ul li a:not(:only-child)","preventDefault","slideToggle","ul","next","open","toggleClass","parent","hasClass","left","getBoundingClientRect",".textrimMenu > .inner > ul > li","querySelector","unset","scrollLeft",".textrimMenu .inner > ul","abs","appendChild","DO NOT remove credit","Theme by <a href="https://www.igniel.com" rel="noopener" target="_blank" title="Igniel">Igniel</a>","innerHTML","<div class="inner"><div class="left">","</div><div class="right">","</div></div></div>","div","className","inner","<div class="left">","</div></div>","right","includes"];
+    let head=document[_0x182c[1]](_0x182c[0])[0]||document[_0x182c[0]],body=document[_0x182c[1]](_0x182c[2])[0]||document[_0x182c[2]];
+    let a=document[_0x182c[4]](_0x182c[3]);
+    a[_0x182c[5]]=_0x182c[6];
+    a[_0x182c[7]]=_0x182c[8];
+    a[_0x182c[9]]=true;
+    a[_0x182c[10]]=function()
+    	{
+    	textrim();
+    	$(document)[_0x182c[16]](function()
+    		{
+    		return $(document)[_0x182c[11]]()>300?$(_0x182c[14])[_0x182c[13]](_0x182c[12]):$(_0x182c[14])[_0x182c[15]](_0x182c[12])
+    	}
+    	),$(_0x182c[14])[_0x182c[20]](function()
+    		{
+    		return $(_0x182c[19])[_0x182c[18]](
+    			{
+    			scrollTop:_0x182c[17]
+    		}
+    		)
+    	}
+    	);
+    	$(_0x182c[27])[_0x182c[26]](_0x182c[25])[_0x182c[24]](
+    		{
+    		'display':_0x182c[21],'visibility':_0x182c[22],'opacity':_0x182c[23]
+    	}
+    	);
+    	$(_0x182c[28])[_0x182c[26]](_0x182c[25])[_0x182c[24]](
+    		{
+    		'visibility':_0x182c[22],'opacity':_0x182c[23]
+    	}
+    	);
+    	$(_0x182c[29])[_0x182c[26]](_0x182c[25])[_0x182c[24]](
+    		{
+    		'visibility':_0x182c[22],'opacity':_0x182c[23]
+    	}
+    	);
+    	let _0xfc89x4=$(_0x182c[30]);
+    	_0xfc89x4[_0x182c[20]](function(a)
+    		{
+    		a[_0x182c[31]]();
+    		$(this)[_0x182c[34]](_0x182c[33])[_0x182c[32]](100);
+    		$(this)[_0x182c[37]]()[_0x182c[36]](_0x182c[35]);
+    		if($(this)[_0x182c[37]]()[_0x182c[38]](_0x182c[35]))
+    			{
+    			let _0xfc89x5=document[_0x182c[42]](_0x182c[41])[_0x182c[40]]()[_0x182c[39]];
+    			$(_0x182c[45])[_0x182c[24]](
+    				{
+    				overflowX:_0x182c[43],left:-Math[_0x182c[46]]($(_0x182c[45])[_0x182c[44]]())
+    			}
+    			)
+    		}
+    		else
+    			{
+    			$(_0x182c[45])[_0x182c[26]](_0x182c[25])
+    		}
+    	}
+    	)
+    };
+    head[_0x182c[47]](a);
+    let footer=document[_0x182c[42]](_0x182c[27]),footerInner=document[_0x182c[42]](_0x182c[28]),footerInnerRight=document[_0x182c[42]](_0x182c[29]),cr1=_0x182c[48],cr2=_0x182c[49];
+    if(!footer)
+    	{
+    	let a=document[_0x182c[4]](_0x182c[27]);
+    	a[_0x182c[50]]=_0x182c[51]+cr1+_0x182c[52]+cr2+_0x182c[53];
+    	body[_0x182c[47]](a)
+    }
+    else
+    	{
+    	if(!footerInner)
+    		{
+    		let a=document[_0x182c[4]](_0x182c[54]);
+    		a[_0x182c[55]]=_0x182c[56];
+    		a[_0x182c[50]]=_0x182c[57]+cr1+_0x182c[52]+cr2+_0x182c[58];
+    		document[_0x182c[42]](_0x182c[27])[_0x182c[47]](a)
+    	}
+    	else
+    		{
+    		if(!footerInnerRight)
+    			{
+    			let a=document[_0x182c[4]](_0x182c[54]);
+    			a[_0x182c[55]]=_0x182c[59];
+    			a[_0x182c[50]]=cr2;
+    			document[_0x182c[42]](_0x182c[28])[_0x182c[47]](a)
+    		}
+    		else
+    			{
+    			if(!footerInnerRight[_0x182c[50]][_0x182c[60]](cr2))
+    				{
+    				footerInnerRight[_0x182c[50]]=cr2
+    			}
+    		}
+    	}
+    }
+
